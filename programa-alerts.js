000f624f7c81de9bd8baedf73a4035a5e9ada537
Amyr Kamel e Silva <amyrkamelesilva@gmail.com>
2021-01-03 17:32:57 -0200

    Primeiro commit js

---
+++ programa-alerts.js
@@ -0,0 +1,9 @@
+window.alert ("Bem vindo ao questionário com JS!")
+
+var nome = window.prompt ("Qual o seu nome?")
+var sobrenome = window.prompt ("Qual o seu sobrenome?")
+var idade = window.prompt ("Quantos anos você tem?")
+
+window.alert (`Parabéns! ${nome} ${sobrenome} de ${idade} anos de idade. Você completou o questionário!` )
+
+
